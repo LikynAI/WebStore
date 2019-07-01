9614e3aec3d47e8a18032ca2a86b7f8c4065dd1c
LikynAI <Likyn2001@gmail.com>
2019-07-01 10:17:37 +0300

    Lesson1 HomeTask

---
+++ WebStore/ViewModels/EmployeeView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore.ViewModels
+{
+	public class EmployeeView
+	{
+		public int Id { get; set; }
+		public string FirsName { get; set; }
+		public string SurName { get; set; }
+		public int Age { get; set; }
+	}
+}
