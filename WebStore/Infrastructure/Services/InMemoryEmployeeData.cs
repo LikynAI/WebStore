f2f98c307fb15c4deeb0d05408996c45265b3e16
LikynAI <Likyn2001@gmail.com>
2019-07-11 00:17:02 +0300

    Добавил возможность редактировать содрудников

---
+++ WebStore/Infrastructure/Services/InMemoryEmployeeData.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Infrastructure.Interfaces;
+using WebStore.ViewModels;
+
+namespace WebStore.Infrastructure.Services
+{
+	public class InMemoryEmployeeData : IEmployeesData
+	{
+		private readonly List<EmployeeView> _employees;
+
+		public InMemoryEmployeeData()
+		{
+			_employees = new List<EmployeeView>
+			{
+				new EmployeeView
+				{
+					Id = 1,
+					FirstName = "Иван",
+					SurName = "Иванов",
+					Age = 27
+				},
+				new EmployeeView
+				{
+					Id = 1,
+					FirstName = "Иван",
+					SurName = "Иванов",
+					Age = 27
+				},
+				new EmployeeView
+				{
+					Id = 1,
+					FirstName = "Иван",
+					SurName = "Иванов",
+					Age = 27
+				}
+			};
+		}
+
+		public void AddNew(EmployeeView model)
+		{
+			model.Id = _employees.Max(x => x.Id) + 1;
+			_employees.Add(model);
+		}
+
+		public void Commit()
+		{
+		}
+
+		public void Delete(int id)
+		{
+			var employee = GetById(id);
+			if (employee != null)
+			{
+				_employees.Remove(employee);
+			}
+		}
+
+		public IEnumerable<EmployeeView> GetAll()
+		{
+			return _employees;
+		}
+
+		public EmployeeView GetById(int id)
+		{
+			return _employees.FirstOrDefault(x => x.Id == id);
+		}
+	}
+}
